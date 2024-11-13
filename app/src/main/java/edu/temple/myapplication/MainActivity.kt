74e4135ce13e2194bb5c3e7ee9dc8a4ba12faea2
bictorg <vj.jimeneznunez@gmail.com>
2024-11-13 15:26:44 -0500

    Implement MainAcitivity

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,57 +1,29 @@
 package edu.temple.myapplication
 
-import android.content.ComponentName
 import android.content.Intent
-import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.IBinder
-import android.os.Looper
 import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
-
-
-
-    var timerBinder : TimerService.TimerBinder? = null
-
-    val serviceConnection = object: ServiceConnection {
-        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-            timerBinder = service as TimerService.TimerBinder
-        }
-
-        override fun onServiceDisconnected(name: ComponentName?) {
-            timerBinder = null
-        }
-
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        bindService(
-            Intent(this, TimerService::class.java),
-            serviceConnection,
-            BIND_AUTO_CREATE
-        )
+        val countdownInput = findViewById<EditText>(R.id.countdownInput)
+        val startButton = findViewById<Button>(R.id.startButton)
 
-        findViewById<Button>(R.id.startButton).setOnClickListener {
-            timerBinder?.start(100)
+        startButton.setOnClickListener {
+            val countdownValue = countdownInput.text.toString().toIntOrNull()
+            if (countdownValue != null) {
+                // Create intent to start the service with the countdown value
+                val serviceIntent = Intent(this, TimerService::class.java).apply {
+                    putExtra("COUNTDOWN_VALUE", countdownValue)
+                }
+                startService(serviceIntent)
+            }
         }
-
-        findViewById<Button>(R.id.pauseButton).setOnClickListener {
-            timerBinder?.pause()
-        }
-        
-        findViewById<Button>(R.id.stopButton).setOnClickListener {
-            timerBinder?.stop()
-        }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        unbindService(serviceConnection)
     }
 }
